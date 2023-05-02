ffda185b24dc57e97d565f00d11a4020a1aa9883
AdityaGaur7 <adgaur027>
2023-05-02 16:34:15 +0530

    traffic

---
+++ script.js
@@ -0,0 +1,10 @@
+function f1(){
+document.getElementById("form").style.display="block";
+document.getElementById("btn1").style.display="none";
+document.getElementById("btn2").style.display="block";
+}
+
+function f2(){
+    document.getElementById("form").style.display="none";
+
+}
